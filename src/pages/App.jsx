72efb9317dcded53a54a62d422a8f17032b30cc0
NeilAutriz <mngautriz@gmail.com>
2025-01-04 16:54:06 +0800

    Fetched data using the axios library

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -10,60 +10,27 @@ import { useEffect, useState } from 'react';
 import PostPage from './PostPage';
 import EditPost from './EditPost';
 
+import api from '../api/blogPosts';
+
+
 // Magcreate ng pangedit 
 
 export default function App() {
   const [posts, setPosts] = useState([])
 
   useEffect(() => {
-    const initialPosts = [
-      {
-        "id": 1,
-        "title": "1st post",
-        "datetime": "July 16, 2021 11:47:39 AM",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "author": "Steve Jobs"
-      },
-      {
-        "id": 2,
-        "title": "Second post",
-        "datetime": "July 16, 2021 11:47:48 AM",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. two",
-        "author": "Steve Jobs"
-      },
-      {
-        "id": 3,
-        "title": "Number Three",
-        "datetime": "July 16, 2021 11:48:01 AM",
-        "body": "Third post... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "author": "Steve Jobs"
-      },
-      {
-        "id": 4,
-        "title": "Testing a 4th post",
-        "datetime": "August 02, 2021 11:46:27 AM",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "author": "Steve Jobs"
-      },
-      {
-        "id": 5,
-        "title": "Fifth Posst",
-        "datetime": "July 16, 2021 11:47:39 AM",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "author": "Steve Jobs"
-      },
-      {
-        "id": 6,
-        "title": "Fifth Posst",
-        "datetime": "July 16, 2021 11:47:39 AM",
-        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "author": "Steve Jobs"
+    const getPosts = async () => {
+      try{
+        const response = await api.get('http://localhost:3500/posts') 
+        setPosts(response.data);
+      } catch (error){
+        console.error(error);
       }
-    ]
-    if(posts.length === 0 && posts){
-      setPosts(initialPosts)
     }
-  }, [posts])
+
+    getPosts();
+
+  }, [])
 
 
   return (
